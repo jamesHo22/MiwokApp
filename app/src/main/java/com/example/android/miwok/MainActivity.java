6bc2e42e465b917ce966304f7d41e16be3271018
James Ho <8.james.ho@gmail.com>
2018-02-03 17:14:48 -0800

    upload to git for macbook

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,10 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,4 +29,37 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
     }
+
+    public void openNumbersActivity (View view){
+
+        Intent openActivity = new Intent(this, NumbersActivity.class);
+        if (openActivity.resolveActivity(getPackageManager()) != null) {
+            startActivity(openActivity);
+        }
+    }
+
+    public void openFamilyMembersActivity (View view){
+
+        Intent openActivity = new Intent(this, FamilyActivity.class);
+        if (openActivity.resolveActivity(getPackageManager()) != null) {
+            startActivity(openActivity);
+        }
+    }
+
+    public void openColorsActivity (View view){
+
+        Intent openActivity = new Intent(this, ColorsActivity.class);
+        if (openActivity.resolveActivity(getPackageManager()) != null) {
+            startActivity(openActivity);
+        }
+    }
+
+    public void openPhrasesActivity (View view){
+
+        Intent openActivity = new Intent(this, PhrasesActivity.class);
+        if (openActivity.resolveActivity(getPackageManager()) != null) {
+            startActivity(openActivity);
+        }
+    }
+
 }
